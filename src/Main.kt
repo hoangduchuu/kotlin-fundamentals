401014014b2fb63d4c495d188ed84c100bb14215
HuuHoang <hoangduchuuvn@gmail.com>
2017-05-21 06:45:51 +0700

    initz

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+import net.hoangduchuu.Person
+
+/**
+ * Created by hoang on 21/05/2017.
+ */
+
+fun printName(name: String){
+    println(" display Name: "+name)
+}
+fun main(args: Array<String>) {
+    println("hello, world");
+
+    var huu = Person("HuuHoang");
+
+    println("Name is :  ${huu.Name}" )
+
+    huu.Name = "Cong Hoang"
+
+    println("Name is :  ${huu.Name}" )
+
+    huu.display()
+
+    huu.displayWithLambda(::printName)
+
+
+}
