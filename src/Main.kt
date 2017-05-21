3f74a54222c3df4e59e364ce068e436a8dff60e2
HuuHoang <hoangduchuuvn@gmail.com>
2017-05-21 07:50:04 +0700

    String template

--- src/Main.kt
+++ src/Main.kt
@@ -4,23 +4,23 @@ import net.hoangduchuu.Person
  * Created by hoang on 21/05/2017.
  */
 
-fun printName(name: String){
-    println(" display Name: "+name)
-}
+
 fun main(args: Array<String>) {
-    println("hello, world");
-
-    var huu = Person("HuuHoang");
-
-    println("Name is :  ${huu.Name}" )
-
-    huu.Name = "Cong Hoang"
-
-    println("Name is :  ${huu.Name}" )
-
-    huu.display()
-
-    huu.displayWithLambda(::printName)
+    println("hello, world")
+    var q = Question()
+    q.Answer = "42"
+    q.Question = ""
 
 
+    q.display();
+    println("the anser is ${q.Answer} ")
+}
+
+class Question {
+    var Answer: String = ""
+    var Question: String = "what is the answer to life?"
+
+    fun display(){
+        println("you said $Answer")
+    }
 }
