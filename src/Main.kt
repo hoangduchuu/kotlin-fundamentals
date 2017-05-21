18a11ed24c8eda9fee2b6c62e686fda9759d622e
HuuHoang <hoangduchuuvn@gmail.com>
2017-05-21 15:00:55 +0700

    io exeption

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,23 @@
 import net.hoangduchuu.Person
+import java.io.FileReader
+import java.io.IOException
 
 /**
  * Created by hoang on 21/05/2017.
  */
 
-fun printName(name: String) {
-    println(" display Name: " + name)
-}
 
 fun main(args: Array<String>) {
     println("hello, world");
 
-    var huu = Person("HuuHoang");
+    var reader = FileReader("filename")
+    try {
+        reader.read()
+    } catch (e: IOException) {
+        e.message
+    }finally {
 
-    println("Name is :  ${huu.Name}")
-
-    huu.Name = "Cong Hoang"
-
-    println("Name is :  ${huu.Name}")
-
-    huu.display()
-
-    huu.displayWithLambda(::printName)
+    }
 
 
 }
