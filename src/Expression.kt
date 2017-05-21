747b8f8dd6e5554e3e0f47dafd7122cf68044e61
HuuHoang <hoangduchuuvn@gmail.com>
2017-05-21 16:10:41 +0700

    function

---
+++ src/Expression.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by hoang on 21/05/2017.
+ */
+
+//fun max(a: Int, b: Int): Int {
+//    if (a > b) {
+//        return a
+//    } else {
+//        return b
+//    }
+//}
+
+ fun max (a: Int, b: Int) : Int  = if (a> b) a else b
+
--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,21 @@
-import net.hoangduchuu.Person
 
 /**
  * Created by hoang on 21/05/2017.
  */
 
+fun display() {
+    println("hello, world")
+}
+
+fun displayWithParam(msg: String): Boolean { // this will print msg and return true
+    println(msg)
+    return true
+}
 
 fun main(args: Array<String>) {
-    println("hello, world");
+    display()
+    displayWithParam("hello again man!")
+
+    println(max(3,2))
 
 }
