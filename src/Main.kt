04df261cc264f19dbb995a5a41d06d4fdb7cc7d8
HuuHoang <hoangduchuuvn@gmail.com>
2017-05-21 14:36:02 +0700

    update src

--- src/Main.kt
+++ src/Main.kt
@@ -4,19 +4,20 @@ import net.hoangduchuu.Person
  * Created by hoang on 21/05/2017.
  */
 
-fun printName(name: String){
-    println(" display Name: "+name)
+fun printName(name: String) {
+    println(" display Name: " + name)
 }
+
 fun main(args: Array<String>) {
     println("hello, world");
 
     var huu = Person("HuuHoang");
 
-    println("Name is :  ${huu.Name}" )
+    println("Name is :  ${huu.Name}")
 
     huu.Name = "Cong Hoang"
 
-    println("Name is :  ${huu.Name}" )
+    println("Name is :  ${huu.Name}")
 
     huu.display()
 
