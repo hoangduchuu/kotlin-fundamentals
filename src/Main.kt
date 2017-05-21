dbc998a89f0b02b3bb0ce3784836a1e6f7200102
HuuHoang <hoangduchuuvn@gmail.com>
2017-05-21 14:31:04 +0700

    for loop

--- src/Main.kt
+++ src/Main.kt
@@ -4,23 +4,13 @@ import net.hoangduchuu.Person
  * Created by hoang on 21/05/2017.
  */
 
-fun printName(name: String){
-    println(" display Name: "+name)
-}
+
 fun main(args: Array<String>) {
-    println("hello, world");
 
-    var huu = Person("HuuHoang");
+    for (i in 1..10) {
+        println("hello, world $i");
 
-    println("Name is :  ${huu.Name}" )
-
-    huu.Name = "Cong Hoang"
-
-    println("Name is :  ${huu.Name}" )
-
-    huu.display()
-
-    huu.displayWithLambda(::printName)
+    }
 
 
 }
